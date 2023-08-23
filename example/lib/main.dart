5f3eef948f0abf5f03edada0f033e6380c631ff4
Imran <imranspahic300@gmail.com>
2023-08-23 11:16:08 +0200

    updated example and gradle versions

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,64 +1,74 @@
+import 'dart:developer';
+import 'package:circle_wheel_scroll/circle_wheel_scroll_view.dart';
 import 'package:flutter/material.dart';
 
-import 'package:circle_wheel_scroll/circle_wheel_scroll_view.dart';
+void main() {
+  runApp(const MyApp());
+}
 
-class WheelExample extends StatelessWidget {
-  Widget _buildItem(int i) {
-    return Center(
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(40),
-        child: Container(
-          width: 80,
-          padding: EdgeInsets.all(20),
-          color: Colors.blue[100 * ((i % 8) + 1)],
-          child: Center(
-            child: Text(
-              i.toString(),
-            ),
-          ),
-        ),
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Circle Wheel Scroll Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: const MyHomePage(title: 'Circle Wheel Scroll Demo'),
     );
   }
+}
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Wheel example'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
       ),
-      body: SafeArea(
-        child: Center(
-          child: Container(
-            // height: 260,
-            // width: 160,
-            child: CircleListScrollView(
-              physics: CircleFixedExtentScrollPhysics(),
-              axis: Axis.vertical,
-              itemExtent: 80,
-              children: List.generate(20, _buildItem),
-              radius: MediaQuery.of(context).size.width * 0.6,
-              onSelectedItemChanged: (int index) =>
-                  print('Current index: $index'),
-            ),
-          ),
+      body: CircleListScrollView(
+        physics: const CircleFixedExtentScrollPhysics(),
+        axis: Axis.vertical,
+        itemExtent: 100,
+        renderChildrenOutsideViewport: true,
+        clipToSize: false,
+        radius: 213,
+        children: List.generate(20, _buildItem),
+        onSelectedItemChanged: (selectedItemChange) {
+          log("CircleWheelList: onSelectedItemChanged = $selectedItemChange");
+        },
+      ),
+    );
+  }
+}
+
+Widget _buildItem(int i) {
+  return Center(
+    child: Container(
+      width: 52,
+      height: 52,
+      decoration: BoxDecoration(
+        color: Colors.blue[100 * ((i % 8) + 1)],
+        borderRadius: BorderRadius.circular(26),
+      ),
+      child: Center(
+        child: Text(
+          i.toString(),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: WheelExample(),
-    );
-  }
-}
-
-void main() => runApp(MyApp());
